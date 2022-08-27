8fb93523addd01a0ded160a46325d23289f01678
mihai-gabriel1 <mr.eu21@gmail.com>
2022-08-27 23:06:13 +0300

    Major remork towards the calculator; went deeper into the dom manipulation and decided to go with data-attributes for this one.

--- script.js
+++ script.js
@@ -1,82 +1,22 @@
-// Variables
-
-const content = document.querySelector("#content");
-const numbers = document.querySelectorAll(".num");
-const equalsBtn = document.getElementById("equals");
-const clearBtn = document.getElementById("ac");
-const changeSign = document.getElementById("plus-minus");
-const operators = document.querySelectorAll(".operator");
-const acReset = document.querySelector(".ac");
-// Events
-
-acReset.addEventListener("click", () => {
-  location.reload();
-});
-
-// Functions
-function addition(a, b) {
-  return a + b;
-}
-
-function subtraction(a, b) {
-  return a - b;
-}
-
-function multiplication(a, b) {
-  return a * b;
-}
-
-function division(a, b) {
-  if (b === 0) {
-    alert("Cannot divide by 0!");
-    clear();
-    current.innerText = "0";
-    return;
-  }
-  return b / y;
-}
-
-function percent(a, b) {
-  return a * (b / 100);
-}
-
-function operate(a, b, c) {
-  let output = 0;
-
-  try {
-    switch (c) {
-      case "+":
-        output = a + b;
-        break;
-
-      case "*":
-        output = a * b;
-        break;
-
-      case "-":
-        output = a - b;
-        break;
-
-      case "/":
-        if (b === 0) {
-          throw "Cant divide with 0!";
-        } else {
-          output = a / b;
-        }
-        break;
-    }
-  } catch (e) {
-    console.log("There's an error"), e;
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
   }
 
-  return output;
-}
-console.log(operate(25, 4, "+"));
+  clear() {}
 
-const digits = document.getElementsByClassName("digit");
-for (let i = 0; i < digits.length; i++) {
-  digits[i].addEventListener("click", function (e) {
-    content.textContent =
-      parseInt(content.textContent) * 10 + parseInt(e.currentTarget.value);
-  });
+  delete() {}
 }
+
+const numberButtons = document.querySelectorAll("[data - number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
